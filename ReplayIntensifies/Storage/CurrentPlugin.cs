9f0d29f9aba091d8bdf657c9070b627dbf7318cc
REHERC <amaury.vanoorenberghe@yahoo.com>
2018-09-09 16:12:33 +0200

    Removed the config from currentplugin.cs

--- ReplayIntensifies/Storage/CurrentPlugin.cs
+++ ReplayIntensifies/Storage/CurrentPlugin.cs
@@ -11,33 +11,8 @@ namespace ReplayIntensifies
         {
             Log = new Logger("ReplayIntensifies.log");
             Log.WriteToConsole = true;
-            Config = new Settings("Config");
-            InitConfig();
-        }
-
-        public static void InitConfig()
-        {
-            var DefaultConfig = new Dictionary<string, object>
-            {
-                {"LanguageFile", ":default:"},
-                {"Rainbow", false},
-                {"Dump", false}
-            };
-
-            foreach (var entry in DefaultConfig)
-            {
-                if (!Config.ContainsKey(entry.Key))
-                {
-                    Config.Add(entry.Key, entry.Value);
-                }
-            }
-
-            FileSystem Files = new FileSystem();
-
-            Config.Save();
         }
 
         public static Logger Log;
-        public static Settings Config;
     }
 }
